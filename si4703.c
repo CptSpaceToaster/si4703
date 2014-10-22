559961fff0d092116267ee684cf1b7da1fb556ff
CptSpaceToaster <Lugia978@yahoo.com>
2014-10-21 21:01:32 -0400

    I hope we're heading in the right direction

--- si4703.c
+++ si4703.c
@@ -2,7 +2,11 @@
  * si4703.c
  *
  * Created: 10/18/2014 3:30:02 PM
- *  Author: CaptainSpaceToaster
+ * Author: CaptainSpaceToaster
+ * 
+ * Some of the defines came from a Nathan Seidle from Sparkfun
+ * I owe him a beer due to the its inclusion (Beerware license).
+ * https://www.sparkfun.com/products/10663
  */ 
 
 #include <stdio.h>
@@ -10,15 +14,52 @@
 #include "../twi/i2cmaster.h"
 #include "util/delay.h"
 
+//data representation of all 32 bytes 
+uint16_t pancakes[16];
+uint8_t reg_index;
+
 void si4703_init() {
-	i2c_start_wait(SI4703_ADDR || I2C_WRITE);
+	si_4703_pull();
+	pancakes[OSCCTRL] = 0x8100;
+	si_4703_push();
+	_delay_ms(500); //Wait for oscillator to settle
+	si_4703_pull();
+	pancakes[POWERCFG] = 0x4001; //Enable the IC
+	pancakes[SYSCONFIG1] |= _BV(RDS); //Enable RDS
+	pancakes[SYSCONFIG2] &= ~(_BV(SPACE1) | _BV(SPACE0)); //FOrce 200 kHz channel spacing (USA)
+	pancakes[SYSCONFIG2] &= ~(0x000F); //Turn down for what?!?
+	pancakes[SYSCONFIG2] |= 0x0001; //Lowest volume setting
+	si_4703_push();
+	_delay_ms(110); //Waiting the max powerup time for the radio
 }
 
 
+
+//The device starts reading at reg_index 0x0A, and has 32 bytes
+void si_4703_pull() {
+	i2c_start_wait(SI4703_ADDR || I2C_READ);
+	for (reg_index=0x00; reg_index<0x10; reg_index++) {
+		pancakes[(reg_index+0x0A) % 0x10] = (i2c_readAck() << 8) | i2c_readAck();
+	}
+	i2c_stop();
+}
+
+
+//The device starts writing at reg_index 4, and there are only 12 bytes that are controlling
+void si_4703_push() {
+	i2c_start_wait(SI4703_ADDR || I2C_WRITE);
+	for (reg_index=0x00; reg_index<0x06; reg_index++) {
+		i2c_write(pancakes[reg_index+2] >> 8);
+		i2c_write(pancakes[reg_index+2] & 0x00FF);
+	}
+	i2c_stop();
+}
+
+/* shudder... shudder shudder shudder shudder 
 void seek_TWI_devices() {
 	uint8_t i = 0; 
 	printf("Looking for devices...\n");
-	for (i=0; i<0x7F; i++) {
+	for (i=0; i<=0x7F; i++) {
 		if (!i2c_start(i<<1)) {
 			printf("There is a Device at 0x%02x", i);
 			i2c_stop();
@@ -27,4 +68,4 @@ void seek_TWI_devices() {
 		_delay_ms(5);
 	}
 	printf("Searched for devices 0x00 to 0x7F\n");
-}
+}  */
--- si4703.h
+++ si4703.h
@@ -6,6 +6,10 @@
  * References: 
  * Programming Guide: http://www.silabs.com/support%20documents/technicaldocs/AN230.pdf
  * Datasheet: https://www.sparkfun.com/datasheets/BreakoutBoards/Si4702-03-C19-1.pdf
+ * 
+ * Some of the defines came from a Nathan Seidle from Sparkfun
+ * I owe him a beer due to the its inclusion (Beerware license).
+ * https://www.sparkfun.com/products/10663
  */
 
 #ifndef SI4703_H
@@ -13,9 +17,53 @@
 
 //definitions
 #define SI4703_ADDR (0x10<<1) //device address shifted over one
+#define SEEK_DOWN  0 //Direction used for seeking. Default is down
+#define SEEK_UP  1
+
+//Define the register names
+#define DEVICEID 0x00
+#define CHIPID  0x01
+#define POWERCFG  0x02
+#define CHANNEL  0x03
+#define SYSCONFIG1  0x04
+#define SYSCONFIG2  0x05
+#define OSCCTRL 0x07
+#define STATUSRSSI  0x0A
+#define READCHAN  0x0B
+#define RDSA  0x0C
+#define RDSB  0x0D
+#define RDSC  0x0E
+#define RDSD  0x0F
+
+//Register 0x02 - POWERCFG
+#define SMUTE  15
+#define DMUTE  14
+#define SKMODE  10
+#define SEEKUP  9
+#define SEEK  8
+
+//Register 0x03 - CHANNEL
+#define TUNE  15
+
+//Register 0x04 - SYSCONFIG1
+#define RDS  12
+#define DE  11
+
+//Register 0x05 - SYSCONFIG2
+#define SPACE1  5
+#define SPACE0  4
+
+//Register 0x0A - STATUSRSSI
+#define RDSR  15
+#define STC  14
+#define SFBL  13
+#define AFCRL  12
+#define RDSS  11
+#define STEREO  8
 
 //functions
 void si4703_init(void);
-void seek_TWI_devices(void);
+void si_4703_pull(void);
+void si_4703_push(void);
 
 #endif //SI4703_H
